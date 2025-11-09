a6ed9475b78dad2fc1d119a68894dcbb2671f5a6
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-08 22:34:19 -0800

    add Ingredient with form data

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = useState<string[]>([]);
+
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(data: FormData) {
+        const newIngredient: string = data.get("ingredient") as string
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input 
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
                     type="text"
-                    placeholder="e.g. rice"
+                    placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul className="ingredient-list">
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
