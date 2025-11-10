67842eec1951519885e2b73bd83a57eeba8e1272
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-10 14:54:14 -0800

    feat: react components for ingredientsList and ClaudeRecipe

---
+++ src/components/IngredientsList.tsx
@@ -0,0 +1,26 @@
+type IngredientsListProps = {
+  ingredients: string[];
+  getRecipe: () => void;
+};
+
+export default function IngredientsList({ingredients, getRecipe}: IngredientsListProps) {
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    return (
+        <section className="ingredients-container">
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredient-list" aria-live="polite">{ingredientsListItems}</ul>
+            {ingredients.length > 3 &&
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button onClick={getRecipe}>Get a recipe</button>
+                </div>
+            }
+        </section>
+    )
+}
