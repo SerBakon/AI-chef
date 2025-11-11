9f6d5b5720afb28b6198d7f8d0cbfbe808276c8a
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-11 14:05:59 -0800

    feat: added css for MARKDOWN recipe

--- src/components/GroqRecipe.tsx
+++ src/components/GroqRecipe.tsx
@@ -2,10 +2,9 @@ import Markdown from "react-markdown"
 
 export default function GroqRecipe({ recipe }:{ recipe: string }) {
     return (
-        <section className="recommendation-container">
+        <section className="recommendation-container" aria-live="polite">
             <h2>Meta Chef Recommends:</h2>
             <article className="suggested-recipe-container" aria-live="polite">
-                {/* {recipe} */}
                 <Markdown>{recipe}</Markdown>
             </article>
         </section>
