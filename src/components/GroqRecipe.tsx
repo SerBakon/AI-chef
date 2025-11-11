e04575e62e40c0b1d3f304fcb6ca056dcdbb92a2
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-11 14:00:18 -0800

    feat: formatted MARKDOWN recipe

--- src/components/GroqRecipe.tsx
+++ src/components/GroqRecipe.tsx
@@ -1,9 +1,12 @@
+import Markdown from "react-markdown"
+
 export default function GroqRecipe({ recipe }:{ recipe: string }) {
     return (
         <section className="recommendation-container">
             <h2>Meta Chef Recommends:</h2>
             <article className="suggested-recipe-container" aria-live="polite">
-                {recipe}
+                {/* {recipe} */}
+                <Markdown>{recipe}</Markdown>
             </article>
         </section>
     )
